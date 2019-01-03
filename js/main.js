e703dff20ff82f7aebeb25ef1d8b85e4a354e6c7
kriskross2018 <46082434+kriskross2018@users.noreply.github.com>
2019-01-04 01:57:50 +0700

    create game logic

--- js/main.js
+++ js/main.js
@@ -30,16 +30,26 @@ var checkForMatch = function () {
 	}
 };
 	
-var flipCard = function (cardId) {
-console.log("User flipped " + cards[cardId].rank);
+var flipCard = function () {
+	cardId = this.getAttribute('data-id');
+	console.log(cardId);
+
 cardsInPlay.push(cards[cardId].rank);
-console.log(cards[cardId].cardImage)
-console.log(cards[cardId].suit)
+this.setAttribute('src', cards[cardId].cardImage);
 if (cardsInPlay.length === 2) {
 	checkForMatch();
 	
 }
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
